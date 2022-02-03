6d6c97123b6e85535215ca38d65f03ac29b8bf76
Brandon <74200568+bsalinassanchez@users.noreply.github.com>
2022-02-03 10:41:22 -0800

    created example java class with method

---
+++ examplemethod.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+public class examplemethod {
+	static String firstLetter(String word) {
+		if(word.length() == 0) {
+			return "";
+		}
+		return word.substring(0,1);
+	}
+	
+
+	public static void main(String[] args) throws IOException {
+		if(args.length ==  0) {
+			System.out.println("no argument");
+		}
+
+		String firstLetterOfWord = examplemethod.firstLetter(args[0]);
+
+		System.out.println(firstLetterOfWord);
+	}
+
+	
+}
